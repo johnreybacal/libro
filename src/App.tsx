19160758023b821a5c096766b497bfa8b10f4ee4
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-10-27 20:24:30 +0800

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,53 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
+    <div className="card bg-base-100 shadow-xl m-5">
+      <div className="card-body">
+      <div className="grid grid-cols-4 gap-4">
+        <div className='grid col-span-3'>
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+        </div>
+        <div className='grid col-span-1'>
         <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <table className="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+            <td>Blue</td>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <th>2</th>
+            <td>Hart Hagerty</td>
+            <td>Desktop Support Technician</td>
+            <td>Purple</td>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <th>3</th>
+            <td>Brice Swyre</td>
+            <td>Tax Accountant</td>
+            <td>Red</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+    </div>
   )
 }
 
