bcdb6215949efda14fdea2e5fa8fafe029ab925e
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-11-30 20:02:01 +0800

    country roads

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
+
 function ErrorPage() {
+  const [isCountryRoads, setIsCountryRoads] = useState(false)
   return <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
     <div className="text-center">
       <h1 className="mt-4 text-5xl font-semibold tracking-tight sm:text-7xl">Something's wrong</h1>
       <p className="mt-6 text-lg font-medium sm:text-xl/8">There's seems to be a problem here.</p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
         <button
-          className="btn btn-primary btn-outline"
+          className="btn btn-primary btn-outline w-32"
           onClick={() => {
             history.back()
           }}
@@ -13,10 +16,12 @@ function ErrorPage() {
           Go back
         </button>
         <a
-          className="btn btn-primary btn-outline"
+          className="btn btn-primary btn-outline w-32"
           href={"/"}
+          onMouseEnter={() => setIsCountryRoads(true)}
+          onMouseLeave={() => setIsCountryRoads(false)}
         >
-          Take me home
+          {isCountryRoads ? "Country roads" : "Take me home"}
         </a>
       </div>
     </div>
